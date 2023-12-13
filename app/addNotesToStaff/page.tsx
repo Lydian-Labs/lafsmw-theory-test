48c9dcc2200c36bf82b8b1efa41dc5829e53a1c0
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-12-13 10:36:34 -0800

    refactored else if statements to an array of objects and used a find method to determine the note

--- app/addNotesToStaff/page.tsx
+++ app/addNotesToStaff/page.tsx
@@ -10,7 +10,10 @@ const AddNotesToAStaff = () => {
   const rendererRef = useRef<InstanceType<typeof Renderer> | null>(null);
   const container = useRef<HTMLDivElement | null>(null);
 
-  type Note = string;
+  interface NoteCoordinate {
+    note: string;
+    yCoordinate: number;
+  }
 
   useEffect(() => {
     if (!rendererRef.current && container.current) {
@@ -33,32 +36,30 @@ const AddNotesToAStaff = () => {
       const rect = container.current?.getBoundingClientRect();
       const x = rect ? e.clientX - rect.left : 0;
       const y = rect ? e.clientY - rect.top : 0;
-      console.log("y", y);
-      console.log("x", x);
+      // console.log("y", y);
+      // console.log("x", x);
 
-      let note: Note = "c/4";
-      const lowEyPosition = 120;
+      const noteArrayYCoordinates: NoteCoordinate[] = [
+        { note: "e/4", yCoordinate: 120 },
+        { note: "f/4", yCoordinate: 115 },
+        { note: "g/4", yCoordinate: 110 },
+        { note: "a/4", yCoordinate: 105 },
+        { note: "b/4", yCoordinate: 100 },
+        { note: "c/5", yCoordinate: 95 },
+        { note: "d/5", yCoordinate: 90 },
+        { note: "e/5", yCoordinate: 85 },
+      ];
 
-      if (y === lowEyPosition) {
-        note = "e/4";
-      } else if (y === lowEyPosition - 5) {
-        note = "f/4";
-      } else if (y === lowEyPosition - 10) {
-        note = "g/4";
-      } else if (y === lowEyPosition - 15) {
-        note = "a/4";
-      } else if (y === lowEyPosition - 20) {
-        note = "b/4";
-      } else if (y === lowEyPosition - 25) {
-        note = "c/5";
-      } else if (y === lowEyPosition - 30) {
-        note = "d/5";
-      } else if (y === lowEyPosition - 35) {
-        note = "e/5";
+      let note = noteArrayYCoordinates.find(
+        ({ yCoordinate }) => yCoordinate === y
+      );
+
+      //Create a new StaveNote with the determined note and add it to the staff
+      if (!note) {
+        throw new Error("Note not found");
       }
-      // Create a new StaveNote with the determined note and add it to the staff
       const newNote = new StaveNote({
-        keys: [note],
+        keys: [note.note],
         duration: "q",
       });
 
