f473cd1df1c033c15576f09cd7d91f5dc56e1cfd
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-12-12 23:51:24 -0800

    commiting all changes from the last week

--- app/addNotesToStaff/page.tsx
+++ app/addNotesToStaff/page.tsx
@@ -4,11 +4,13 @@ import React, { useRef, useEffect } from "react";
 import VexFlow, { Accidental } from "vexflow";
 
 const VF = VexFlow.Flow;
-const { Formatter, Renderer, Stave, StaveNote } = VF;
+const { Formatter, Renderer, Stave, StaveNote, StaveModifier, Barline } = VF;
 
 const AddNotesToAStaff = () => {
   const rendererRef = useRef<InstanceType<typeof Renderer> | null>(null);
   const container = useRef<HTMLDivElement | null>(null);
+
+  type Note = string;
 
   useEffect(() => {
     if (!rendererRef.current && container.current) {
@@ -19,41 +21,41 @@ const AddNotesToAStaff = () => {
     }
 
     const renderer = rendererRef.current;
-    renderer ? renderer.resize(500, 500) : null;
+    renderer?.resize(500, 500);
     const context = renderer ? renderer.getContext() : null;
-    context ? context.setFont("Arial", 10) : null;
+    context?.setFont("Arial", 10);
     const stave = new Stave(10, 40, 400);
+    stave.setEndBarType(3);
     stave.addClef("treble").addTimeSignature("4/4");
     context ? stave.setContext(context).draw() : null;
+
     container.current?.addEventListener("click", (e) => {
       const rect = container.current?.getBoundingClientRect();
       const x = rect ? e.clientX - rect.left : 0;
       const y = rect ? e.clientY - rect.top : 0;
-      console.log("rect", rect, "x", x);
-      const determineNoteFromPosition = (x: number, y: number) => {
-        const notePositions = {
-          "e/5": 40,
-          "d/5": 45,
-          "c/5": 50,
-          "b/4": 55,
-          "a/4": 60,
-          "g/4": 65,
-          "f/4": 70,
-          "e/4": 75,
-          "d/4": 80,
-        };
-        let closestNote = 40;
-        let smallestDifference = Math.abs(y - closestNote);
-        for (const note in notePositions) {
-          const difference = Math.abs(y - closestNote);
-          if (difference < smallestDifference) {
-            smallestDifference = difference;
-            closestNote = note;
-          }
-        }
-        return closestNote;
-      };
-      const note = determineNoteFromPosition(x, y);
+      console.log("y", y);
+      console.log("x", x);
+
+      let note: Note = "c/4";
+      const lowEyPosition = 120;
+
+      if (y === lowEyPosition) {
+        note = "e/4";
+      } else if (y === lowEyPosition - 5) {
+        note = "f/4";
+      } else if (y === lowEyPosition - 10) {
+        note = "g/4";
+      } else if (y === lowEyPosition - 15) {
+        note = "a/4";
+      } else if (y === lowEyPosition - 20) {
+        note = "b/4";
+      } else if (y === lowEyPosition - 25) {
+        note = "c/5";
+      } else if (y === lowEyPosition - 30) {
+        note = "d/5";
+      } else if (y === lowEyPosition - 35) {
+        note = "e/5";
+      }
       // Create a new StaveNote with the determined note and add it to the staff
       const newNote = new StaveNote({
         keys: [note],
