0587243ee4e5abd1141780d877c3f9d12f6592a0
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-12-13 10:52:41 -0800

    refactored yCoordinates min and max

--- app/addNotesToStaff/page.tsx
+++ app/addNotesToStaff/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { notDeepEqual } from "assert";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import VexFlow, { Accidental } from "vexflow";
 
 const VF = VexFlow.Flow;
@@ -9,10 +9,15 @@ const { Formatter, Renderer, Stave, StaveNote, StaveModifier, Barline } = VF;
 const AddNotesToAStaff = () => {
   const rendererRef = useRef<InstanceType<typeof Renderer> | null>(null);
   const container = useRef<HTMLDivElement | null>(null);
+  const [createNote, setCreateNote] = useState({
+    note: "e/4",
+    yCoordinate: 119 || 120 || 121,
+  });
 
   interface NoteCoordinate {
     note: string;
-    yCoordinate: number;
+    yCoordinateMin: number;
+    yCoordinateMax: number;
   }
 
   useEffect(() => {
@@ -36,22 +41,21 @@ const AddNotesToAStaff = () => {
       const rect = container.current?.getBoundingClientRect();
       const x = rect ? e.clientX - rect.left : 0;
       const y = rect ? e.clientY - rect.top : 0;
-      // console.log("y", y);
-      // console.log("x", x);
 
       const noteArrayYCoordinates: NoteCoordinate[] = [
-        { note: "e/4", yCoordinate: 120 },
-        { note: "f/4", yCoordinate: 115 },
-        { note: "g/4", yCoordinate: 110 },
-        { note: "a/4", yCoordinate: 105 },
-        { note: "b/4", yCoordinate: 100 },
-        { note: "c/5", yCoordinate: 95 },
-        { note: "d/5", yCoordinate: 90 },
-        { note: "e/5", yCoordinate: 85 },
+        { note: "e/4", yCoordinateMin: 119, yCoordinateMax: 121 },
+        { note: "f/4", yCoordinateMin: 114, yCoordinateMax: 116 },
+        { note: "g/4", yCoordinateMin: 109, yCoordinateMax: 111 },
+        { note: "a/4", yCoordinateMin: 104, yCoordinateMax: 106 },
+        { note: "b/4", yCoordinateMin: 99, yCoordinateMax: 101 },
+        { note: "c/5", yCoordinateMin: 95, yCoordinateMax: 96 },
+        { note: "d/5", yCoordinateMin: 89, yCoordinateMax: 91 },
+        { note: "e/5", yCoordinateMin: 84, yCoordinateMax: 86 },
       ];
 
       let note = noteArrayYCoordinates.find(
-        ({ yCoordinate }) => yCoordinate === y
+        ({ yCoordinateMin, yCoordinateMax }) =>
+          y >= yCoordinateMin && y <= yCoordinateMax
       );
 
       //Create a new StaveNote with the determined note and add it to the staff
