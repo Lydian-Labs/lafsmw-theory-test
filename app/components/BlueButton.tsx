21b8aafe4f038d862e29e282d2ed37a360a4c16f
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-12-23 17:27:19 -0800

    added a custom blue button component to the components folder

---
+++ app/components/BlueButton.tsx
@@ -0,0 +1,35 @@
+import { Button, styled } from "@mui/material/";
+import React, { ReactNode, MouseEventHandler } from "react";
+
+const BlueButtonStyled = styled(Button)({
+  "&&": {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: "#0063cc",
+    borderColor: "#0063cc",
+  },
+  "&:hover": {
+    backgroundColor: "#0069d9",
+    borderColor: "#0062cc",
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
+
+interface BlueButtonProps {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  children: ReactNode;
+}
+
+const BlueButton: React.FC<BlueButtonProps> = ({ onClick, children }) => {
+  return (
+    <BlueButtonStyled variant="contained" onClick={onClick}>
+      {children}
+    </BlueButtonStyled>
+  );
+};
+
+export default BlueButton;
