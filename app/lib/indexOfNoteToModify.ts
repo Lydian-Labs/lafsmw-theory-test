348df998a746ed7c27d9a4e0de4eb69c5f88c834
Kasey Knudsen <kaseyknudsen@gmail.com>
2024-01-14 22:25:40 -0800

    moved indexOfNoteToModify to a separate file in lib

---
+++ app/lib/indexOfNoteToModify.ts
@@ -0,0 +1,15 @@
+import {
+  NoteStringAndYMinAndYMax,
+  StaveNoteAndXAndYCoordinates,
+} from "../typesAndInterfaces";
+
+export const indexOfNoteToModify = (
+  staveData: StaveNoteAndXAndYCoordinates[],
+  noteObj: NoteStringAndYMinAndYMax
+): number => {
+  const index: number = staveData?.findIndex(
+    (note) =>
+      note.y >= noteObj.yCoordinateMin && note.y <= noteObj.yCoordinateMax
+  );
+  return index;
+};
