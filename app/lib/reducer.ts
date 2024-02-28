d00ae9665466280a1f84b1e284ad98e148c88853
Kasey Knudsen <kaseyknudsen@gmail.com>
2024-02-27 22:34:40 -0800

    refactor reducer code

--- app/lib/reducer.ts
+++ app/lib/reducer.ts
@@ -1,68 +1,14 @@
 import { StateType, Action } from "./typesAndInterfaces";
 
-export const reducer = (state: StateType, action: Action): StateType => {
-  switch (action.type) {
-    case "isEnterNoteActive":
-      return {
-        ...state,
-        isEnterNoteActive: true,
-        isEraseNoteActive: false,
-        isChangeNoteActive: false,
-        isSharpActive: false,
-        isFlatActive: false,
-        isEraseAccidentalActive: false,
-      };
-    case "isEraseNoteActive":
-      return {
-        ...state,
-        isEnterNoteActive: false,
-        isEraseNoteActive: true,
-        isChangeNoteActive: false,
-        isSharpActive: false,
-        isFlatActive: false,
-        isEraseAccidentalActive: false,
-      };
-    case "isChangeNoteActive":
-      return {
-        ...state,
-        isEnterNoteActive: false,
-        isEraseNoteActive: false,
-        isChangeNoteActive: true,
-        isSharpActive: false,
-        isFlatActive: false,
-        isEraseAccidentalActive: false,
-      };
-    case "isSharpActive":
-      return {
-        ...state,
-        isEnterNoteActive: false,
-        isEraseNoteActive: false,
-        isChangeNoteActive: false,
-        isSharpActive: true,
-        isFlatActive: false,
-        isEraseAccidentalActive: false,
-      };
-    case "isFlatActive":
-      return {
-        ...state,
-        isEnterNoteActive: false,
-        isEraseNoteActive: false,
-        isChangeNoteActive: false,
-        isSharpActive: false,
-        isFlatActive: true,
-        isEraseAccidentalActive: false,
-      };
-    case "isEraseAccidentalActive":
-      return {
-        ...state,
-        isEnterNoteActive: false,
-        isEraseNoteActive: false,
-        isChangeNoteActive: false,
-        isSharpActive: false,
-        isFlatActive: false,
-        isEraseAccidentalActive: true,
-      };
-    default:
-      return state;
+export const reducer = (state: StateType, action: Action) => {
+  const newState = Object.keys(state).reduce((result, key) => {
+    result[key] = false;
+    return result;
+  }, {} as StateType);
+
+  if (action.type in newState) {
+    newState[action.type] = true;
   }
+
+  return newState;
 };
