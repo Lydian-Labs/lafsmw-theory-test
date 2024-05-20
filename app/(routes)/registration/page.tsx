2aeb2864e95db9a2fa05c16bb96e3bd76514bb28
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-05-20 14:26:47 -0700

    feat(emailForgotPW): Refactor registration form CSS.

--- app/(routes)/registration/page.tsx
+++ app/(routes)/registration/page.tsx
@@ -5,6 +5,8 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import SignInForm from "../../components/SignInForm";
 import SignUpForm from "../../components/SignUpForm";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 export default function Registration() {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -29,41 +31,39 @@ export default function Registration() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Stack spacing={4}>
-        <Stack spacing={8}>
-          {showButtons && (
-            <Stack spacing={2}>
-              <Typography>New to LAFSMW? Sign up here:</Typography>
-              <Button variant="contained" onClick={toggleIsSignUp}>
-                Sign Up
-              </Button>
-            </Stack>
-          )}
+      <Stack spacing={8}>
+        {showButtons && (
+          <Stack spacing={2}>
+            <Typography>New to LAFSMW? Sign up here:</Typography>
+            <Button variant="contained" onClick={toggleIsSignUp}>
+              Sign Up
+            </Button>
+          </Stack>
+        )}
 
-          {showButtons && (
-            <Stack spacing={2}>
-              <Typography>Returning student? Sign in here:</Typography>
-              <Button variant="contained" onClick={toggleIsSignIn}>
-                Sign In
-              </Button>
-            </Stack>
-          )}
-        </Stack>
+        {showButtons && (
+          <Stack spacing={2}>
+            <Typography>Returning student? Sign in here:</Typography>
+            <Button variant="contained" onClick={toggleIsSignIn}>
+              Sign In
+            </Button>
+          </Stack>
+        )}
         {isSignUp && (
-          <>
+          <Stack spacing={4}>
             <SignUpForm />
             <Button variant="text" onClick={goBack}>
               Go Back
             </Button>
-          </>
+          </Stack>
         )}
         {isSignIn && (
-          <>
+          <Grid xs={12} container justifyContent="center" gap={4}>
             <SignInForm />
-            <Button variant="text" onClick={goBack}>
+            <Button variant="text" onClick={goBack} sx={{ width: "73%" }}>
               Go Back
             </Button>
-          </>
+          </Grid>
         )}
       </Stack>
     </main>
