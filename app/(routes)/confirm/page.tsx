1a7c76c2dd016816d45fb762d183f445a6827287
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-06-22 13:05:18 -0700

    fix(authRefactor4): Model the design of the error page in confirm to look like app button design.

--- app/(routes)/confirm/page.tsx
+++ app/(routes)/confirm/page.tsx
@@ -1,10 +1,10 @@
 "use client";
+import UpdateName from "@/app/components/UpdateName";
 import { completeSignIn } from "@/firebase/authAPI";
-import { Stack, Typography } from "@mui/material";
+import { Button, Container, Grid, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import UpdateName from "@/app/components/UpdateName";
 
 export default function ConfirmSignIn() {
   const router = useRouter();
@@ -15,7 +15,6 @@ export default function ConfirmSignIn() {
   useEffect(() => {
     const handleSignIn = async () => {
       const emailLink = window.location.href;
-      console.log("emailLink from confirm:", emailLink);
 
       try {
         const success = await completeSignIn(emailLink);
@@ -49,43 +48,23 @@ export default function ConfirmSignIn() {
 
   if (error) {
     return (
-      <>
+      <Container sx={{ paddingTop: "44px" }}>
         <Typography variant="h6" align="center" p={4}>
           {error}
         </Typography>
-        <Link href="/login">
-          <Stack alignItems={"center"}>
-            <Typography
-              variant="body1"
-              width={"190px"}
-              border={"1px solid"}
-              borderRadius={"12px"}
-              p={4}
-              sx={{
-                "&:hover": { color: "var(--primary40)", border: "none" },
-              }}
-            >
-              Return to login
-            </Typography>
-          </Stack>
-        </Link>
-        <Link href="/registration">
-          <Stack alignItems={"center"}>
-            <Typography
-              variant="body1"
-              width={"190px"}
-              border={"1px solid"}
-              borderRadius={"12px"}
-              p={4}
-              sx={{
-                "&:hover": { color: "var(--primary40)", border: "none" },
-              }}
-            >
+        <Stack spacing={4} alignItems={"center"}>
+          <Link href="/login">
+            <Button variant="text" sx={{ width: "250px" }}>
+              Try again
+            </Button>
+          </Link>
+          <Link href="/registration">
+            <Button variant="text" sx={{ width: "250px" }}>
               Or sign up here
-            </Typography>
-          </Stack>
-        </Link>
-      </>
+            </Button>
+          </Link>
+        </Stack>
+      </Container>
     );
   }
 
