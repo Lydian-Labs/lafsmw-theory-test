883a106d3dd561229ed149b5589ed7f00780124d
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-06-24 12:02:15 -0700

    feat(tutorialRefactor): Remove KeySigNotate1 from ExamPages due to it being redundant. Create TutorialModal component. Refactor KeySigNotateTemplate to use TutorialModal, removing TutorialCard. Made Notate key signature box larger for future accomodation of larger stave.

--- app/components/ExamPages/KeySigNotateTemplate.tsx
+++ app/components/ExamPages/KeySigNotateTemplate.tsx
@@ -2,11 +2,13 @@
 import { keySigNotationInstructions } from "@/app/lib/data/instructions";
 import keySignaturesText from "@/app/lib/data/keySignaturesText";
 import { MouseEvent, UserDataProps } from "@/app/lib/typesAndInterfaces";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Modal, Typography } from "@mui/material";
 import { useState } from "react";
 import CardFooter from "../CardFooter";
 import NotateKeySignature from "../NotateKeySignature";
 import TutorialCard from "../TutorialCard";
+import TutorialModal from "../TutorialModal";
+import { display } from "html2canvas/dist/types/css/property-descriptors/display";
 
 export default function KeySignaturesNotation({
   currentUserData,
@@ -31,8 +33,20 @@ export default function KeySignaturesNotation({
     setKeySignatureNotation(input);
   }
 
+  const boxStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  };
+
   return (
     <Container>
+      <Box sx={boxStyle}>
+        <Typography variant="h5" align="center" pb={2}>
+          Section 1: Notate Key Signatures
+        </Typography>
+        <TutorialModal tutorialInstructions={keySigNotationInstructions} />
+      </Box>
       <Box
         component="main"
         width={1139}
@@ -43,16 +57,9 @@ export default function KeySignaturesNotation({
         boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
       >
         <Grid container spacing={4} p={2}>
-          <Grid item xs={4}>
-            <TutorialCard
-              tutorialInstructions={keySigNotationInstructions}
-              firstPage={page === 1}
-              title={"Section 1: Notate Key Signatures"}
-            />
-          </Grid>
-          <Grid item xs={8} margin={"auto"}>
+          <Grid item xs={12} margin={"auto"}>
             <Box
-              width={569}
+              width={750}
               height={540}
               bgcolor={"card.background"}
               borderRadius="var(--borderRadius)"
---
+++ app/components/TutorialModal.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import { Stack, List, ListItem, ListItemText } from "@mui/material";
+
+type TutorialInstructions = {
+  instructionTitle: string;
+  instructionText: string;
+};
+
+interface TutorialModalProps {
+  tutorialInstructions: TutorialInstructions[];
+}
+
+export default function TutorialModal({
+  tutorialInstructions,
+}: TutorialModalProps) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button variant={"contained"} onClick={handleOpen}>
+        Tutorial
+      </Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          width={600}
+          height={456}
+          bgcolor={"card.background"}
+          borderRadius="var(--borderRadius)"
+          boxShadow="var(--cardShadow)"
+          position={"absolute"}
+          top={"25%"}
+          left={"28%"}
+        >
+          <Stack mx={3} p={1}>
+            <Typography variant="h6" align="center">
+              Tutorial
+            </Typography>
+            <List>
+              {tutorialInstructions.map((value, index) => (
+                <ListItem key={index} disableGutters>
+                  <ListItemText
+                    primary={`${index + 1}. ${value.instructionTitle}`}
+                    secondary={value.instructionText}
+                    primaryTypographyProps={{ fontSize: "16px" }}
+                    secondaryTypographyProps={{ fontSize: "16px" }}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Stack>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
