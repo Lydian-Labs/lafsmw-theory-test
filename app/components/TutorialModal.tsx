01d34f00273bcb1d217de4cb572f177028273508
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-06-24 22:49:18 -0700

    feat(tutorialRefactor): Make TutorialModal button size large. Make uppercase the Add Flat and Add Sharp in actionTypes.

--- app/components/TutorialModal.tsx
+++ app/components/TutorialModal.tsx
@@ -24,7 +24,7 @@ export default function TutorialModal({
 
   return (
     <div>
-      <Button variant={"contained"} onClick={handleOpen}>
+      <Button variant={"contained"} onClick={handleOpen} size="large">
         Need Help?
       </Button>
       <Modal open={open} onClose={handleClose}>
