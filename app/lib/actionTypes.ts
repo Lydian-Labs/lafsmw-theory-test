8fe6c605e02d6e0612c94080c79a5875149ee3db
Kasey Knudsen <kaseyknudsen@gmail.com>
2024-09-02 11:40:21 -0700

    fix: add back in Erase Accidental button

--- app/lib/actionTypes.ts
+++ app/lib/actionTypes.ts
@@ -17,4 +17,5 @@ export const modifyChordsActionTypes = {
 export const modifyKeySigActionTypes = {
   isSharpActive: "Add Sharp",
   isFlatActive: "Add Flat",
+  isEraseAccidentalActive: "Erase Accidental"
 };
